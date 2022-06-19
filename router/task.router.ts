7a61e6979566d85ee22b19451cadda74db99779a
Michal <83036436+naukagit1@users.noreply.github.com>
2022-06-19 16:21:35 +0200

    Added all Endpoints

---
+++ router/task.router.ts
@@ -0,0 +1,33 @@
+import {Router} from "express";
+import {TaskRecord} from "../records/task.record";
+
+export const taskRouter = Router()
+    .get('/', async (req, res) => {
+        const tasks = await TaskRecord.listAll()
+        res.json(tasks)
+    })
+    .get('/:id', async (req, res) => {
+        const taskId = await TaskRecord.getOne(req.params.id)
+        res.json(taskId)
+    })
+    .patch('/:id', async(req,res)=>{
+        const updateTask = await TaskRecord.getOne(req.params.id);
+        const data = {
+            id: req.params.id,
+            ...req.body,
+            taskTodo: req.body.taskTodo
+        }
+        const newTask = new TaskRecord(data)
+        await newTask.update()
+        res.json(updateTask)
+    })
+    .delete('/:id/', async (req, res) => {
+        const taskDelete = await TaskRecord.delete(req.params.id);
+        res.json(taskDelete)
+
+    })
+    .post('/add/', async(req,res)=>{
+       const addTask = new TaskRecord(req.body)
+        await addTask.insert()
+        res.json(addTask)
+    })
