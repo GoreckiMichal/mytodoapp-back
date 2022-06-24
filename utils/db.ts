01580bce0f1efaa360fe259a045aee8a8e2953c8
Michal <83036436+naukagit1@users.noreply.github.com>
2022-06-24 16:27:05 +0200

    Added config example and move DB connection data into config

--- utils/db.ts
+++ utils/db.ts
@@ -1,10 +1,11 @@
 import {createPool} from "mysql2/promise";
+import {config} from "../config/config";
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'megak_todoapp',
+    host: config.dbHost,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbDatabase,
     namedPlaceholders: true,
     decimalNumbers: true,
     dateStrings:true,
